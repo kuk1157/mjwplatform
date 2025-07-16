fda15f819825277185e54fb93603e4b33c736183
mjw7 <mjw@vpudding.com>
2025-07-16 11:29:21 +0900

    Add: 백엔드 - 결제, 결제내역 entity 세팅

---
+++ backend/src/main/java/com/pudding/base/domain/pay/entity/Pay.java
@@ -0,0 +1,50 @@
+package com.pudding.base.domain.pay.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Table(name = "pay") // pay 테이블 (결제 테이블)
+public class Pay {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Schema(description = "고유번호")
+    private Long id;
+
+    @Column(name = "order_id")
+    @Schema(description = "주문 고유번호")
+    private Integer orderId;
+
+    @Column(name = "store_id")
+    @Schema(description = "매장 고유번호")
+    private Integer storeId;
+
+    @Column(name = "owner_id")
+    @Schema(description = "점주 고유번호")
+    private Integer ownerId;
+
+    @Column(name = "user_id")
+    @Schema(description = "고객 고유번호")
+    private Integer userId;
+
+    @Column(name = "total_amount")
+    @Schema(description = "합계 결제금액")
+    private String totalAmount;
+
+    @Column(name = "discount_amount")
+    @Schema(description = "할인된 결제금액")
+    private String discountAmount;
+
+    @Column(name = "final_amount")
+    @Schema(description = "최종 결제금액")
+    private String finalAmount;
+
+    @Column(name = "paid_at")
+    @Schema(description = "결제 완료일")
+    private LocalDateTime paidAt; // 결제가 최종 성공했을 경우(현재로는 서버 이슈, 세션이슈 등 그런상황 발생시엔 남으면안됨.)
+
+}
---
+++ backend/src/main/java/com/pudding/base/domain/payLog/entity/PayLog.java
@@ -0,0 +1,25 @@
+package com.pudding.base.domain.payLog.entity;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Table(name = "pay_log") // pay_log 테이블 (결제내역 테이블)
+public class PayLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Schema(description = "고유번호")
+    private Long id;
+
+    @Column(name = "pay_id")
+    @Schema(description = "결제 고유번호")
+    private Integer payId;
+
+    @Column(name = "pay_type")
+    @Schema(description = "결제 타입")
+    private String payType; // 추후 enum 으로 활용
+
+}
