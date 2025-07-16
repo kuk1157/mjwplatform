20d3dd8be69a461cf9615818d7b09dbb32877b78
mjw7 <mjw@vpudding.com>
2025-07-16 11:06:36 +0900

    Add: 백엔드 - 매장테이블, 플랫폼설정 entity 세팅
    - 플랫폼 설정 entity는 3%적립률, 100만원, 3만포인트 한도를 정하여 플랫폼 전체에서 공통으로 활용하기 위한 entity

---
+++ backend/src/main/java/com/pudding/base/domain/platformConfig/entity/platformConfig.java
@@ -0,0 +1,30 @@
+package com.pudding.base.domain.platformConfig.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Table(name = "platform_config") // platform_config 테이블 (플랫폼 설정 테이블)
+public class platformConfig {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Schema(description = "고유번호")
+    private Long id;
+
+    @Column(name = "point_rate")
+    @Schema(description = "기본 포인트 적립률")
+    private Integer pointRate;
+
+    @Column(name = "max_store_price")
+    @Schema(description = "매장당 하루 최대 주문금액")
+    private Integer maxStorePrice;
+
+    @Column(name = "max_store_point")
+    @Schema(description = "매장당 하루 최대 포인트")
+    private Integer maxStorePoint;
+
+
+}
---
+++ backend/src/main/java/com/pudding/base/domain/storeTable/entity/storeTable.java
@@ -0,0 +1,26 @@
+package com.pudding.base.domain.storeTable.entity;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Table(name = "store_table") // // store_table 테이블 (매장테이블 테이블)
+public class storeTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Schema(description = "고유번호")
+    private Long id;
+
+    @Column(name = "store_id")
+    @Schema(description = "매장 고유번호")
+    private Integer storeId;
+
+    @Column(name = "table_number")
+    @Schema(description = "테이블 번호")
+    private Integer tableNumber;
+
+}
