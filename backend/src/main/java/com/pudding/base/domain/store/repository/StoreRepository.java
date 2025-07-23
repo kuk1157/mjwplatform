92d641ac498bc4d031c793032db4fdfed5cb7782
mjw7 <mjw@vpudding.com>
2025-07-23 12:42:42 +0900

    Fix: 백엔드 - 매장(store) repository에 쿼리메서드 추가
    - 매장리스트 select join문

--- backend/src/main/java/com/pudding/base/domain/store/repository/StoreRepository.java
+++ backend/src/main/java/com/pudding/base/domain/store/repository/StoreRepository.java
@@ -1,10 +1,31 @@
 package com.pudding.base.domain.store.repository;
 
+import com.pudding.base.domain.store.dto.StoreDto;
 import com.pudding.base.domain.store.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer> {
     Store findByOwnerId(Integer memberId);
+
+
+    @Query("""
+    SELECT new com.pudding.base.domain.store.dto.StoreDto(
+        s.id, s.ownerId, s.name, s.address, m.name, s.createdAt
+    )
+    FROM Store s
+    JOIN Member m ON s.ownerId = m.id
+    WHERE (:keyword IS NULL OR 
+           LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR 
+           LOWER(s.address) LIKE LOWER(CONCAT('%', :keyword, '%')) OR 
+           LOWER(m.name) LIKE LOWER(CONCAT('%', :keyword, '%')))
+""")
+    Page<StoreDto> findByStoreSearch(Pageable pageable, @Param("keyword") String keyword);
+
+
 }
