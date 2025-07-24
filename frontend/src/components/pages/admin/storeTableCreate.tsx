8fe4f0b441a048397c8122c053cc7a757120fc8b
mjw7 <mjw@vpudding.com>
2025-07-24 10:22:03 +0900

    Add: 프론트엔드 - 매장테이블 전체 페이지 생성
    - 매장테이블 목록, 등록, 상세보기
    - 수정,삭제는 제외.(추후 삭제만 검토중)

---
+++ frontend/src/components/pages/admin/storeTableCreate.tsx
@@ -0,0 +1,7 @@
+import { SectionCard } from "../../molecules/card";
+
+export function StoreTableCreate() {
+    return <SectionCard className="px-[30px]"></SectionCard>;
+}
+
+export default StoreTableCreate;
---
+++ frontend/src/components/pages/admin/storeTableDetail.tsx
@@ -0,0 +1,7 @@
+import { SectionCard } from "../../molecules/card";
+
+export function StoreTableDetail() {
+    return <SectionCard className="px-[30px]"></SectionCard>;
+}
+
+export default StoreTableDetail;
