bf5cc47dc9f81f349802183939a7da9c83568b90
mjw7 <mjw@vpudding.com>
2025-07-29 14:26:49 +0900

    Fix: 백엔드 - 특정 고객 nft 전체 조회 - 마이페이지용도

--- backend/src/main/java/com/pudding/base/domain/nft/repository/NftRepository.java
+++ backend/src/main/java/com/pudding/base/domain/nft/repository/NftRepository.java
@@ -4,6 +4,9 @@ import com.pudding.base.domain.nft.entity.Nft;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NftRepository extends JpaRepository<Nft, Integer> {
+    List<Nft> findByCustomerId(Integer customerId);
 }
--- backend/src/main/java/com/pudding/base/domain/nft/service/NftServiceImpl.java
+++ backend/src/main/java/com/pudding/base/domain/nft/service/NftServiceImpl.java
@@ -4,10 +4,13 @@ package com.pudding.base.domain.nft.service;
 import com.pudding.base.domain.nft.dto.NftDto;
 import com.pudding.base.domain.nft.entity.Nft;
 import com.pudding.base.domain.nft.repository.NftRepository;
+import com.pudding.base.domain.storeTable.entity.StoreTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,4 +30,18 @@ public class NftServiceImpl implements NftService {
         return NftDto.fromEntity(savedNft);
 
     }
+
+
+    public List<NftDto> getAllNft(Integer customerId){
+        List<Nft> nfts = nftRepository.findByCustomerId(customerId);
+
+        return nfts.stream().map(nft -> NftDto.builder()
+                .id(nft.getId())
+                .tokenId(nft.getTokenId())
+                .storeId(nft.getStoreId())
+                .customerId(nft.getCustomerId())
+                .createdAt(nft.getCreatedAt())
+                .build()).collect(Collectors.toList());
+    }
+
 }
