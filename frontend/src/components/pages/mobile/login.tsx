1dd62a961bbe2eda8cb73104fceb6f6ddb6c3745
mjw7 <mjw@vpudding.com>
2025-08-04 09:54:14 +0900

    Fix: 프론트엔드 - 방문기록 전체 프로세스 한 api로 통일
    - 프론트에서 여러개 api 호출하던거 1개로 변경

--- frontend/src/components/pages/mobile/login.tsx
+++ frontend/src/components/pages/mobile/login.tsx
@@ -7,10 +7,11 @@ export function MobileLogin() {
     const { storeNum, tableNumber } = useParams();
 
     const LoginSubmit = async () => {
-        const did = "did:testdid3"; // 현재 임시로 입력해줘야함. 다대구 did 연동하게 되면 해결
+        const did = "did:testdid4"; // 현재 임시로 입력해줘야함. 다대구 did 연동하게 되면 해결
         try {
             // 방문기록 api
             // 첫 방문일 경우 계정 생성도 동시에 이뤄짐
+            // nft 발급 로직까지 전체 진행
             const url = `/api/v1/visits/${storeNum}/${tableNumber}`;
             const orderData = {
                 tableNumber,
@@ -22,27 +23,26 @@ export function MobileLogin() {
             console.log(response.data);
 
             // [ 여기 모바일 로그인 로직 추가 해야함 ]
-
             const customerId = response.data.customerId; // 고객 번호 가공
 
-            // nft 중복 발급 체크위한 API
-            const nftCheckUrl = `/api/v1/customers/${customerId}/stores/${storeNum}/nfts/check`;
-            const resCheck = await axios.get(nftCheckUrl);
-            console.log(resCheck.data);
+            // // nft 중복 발급 체크위한 API
+            // const nftCheckUrl = `/api/v1/customers/${customerId}/stores/${storeNum}/nfts/check`;
+            // const resCheck = await axios.get(nftCheckUrl);
+            // console.log(resCheck.data);
 
-            // nft 중복 발급 막기
-            if (resCheck.data == false) {
-                // 로그인 성공 후 NFT발급 진행
-                const nftUrl = `/api/v1/stores/${storeNum}/customers/${customerId}/nfts`;
-                const nftData = {
-                    did,
-                };
-                const res = await axios.post(nftUrl, nftData);
-                console.log("nft발급 성공");
-                console.log(res.data);
-            } else {
-                console.log("이미 발급된 nft 매장 바로 마이페이지로");
-            }
+            // // nft 중복 발급 막기
+            // if (resCheck.data == false) {
+            //     // 로그인 성공 후 NFT발급 진행
+            //     const nftUrl = `/api/v1/stores/${storeNum}/customers/${customerId}/nfts`;
+            //     const nftData = {
+            //         did,
+            //     };
+            //     const res = await axios.post(nftUrl, nftData);
+            //     console.log("nft발급 성공");
+            //     console.log(res.data);
+            // } else {
+            //     console.log("이미 발급된 nft 매장 바로 마이페이지로");
+            // }
 
             // 마이 페이지에서 nft 정보 호출을 위해 customerId 활용
             console.log("마이페이지로 이동");
