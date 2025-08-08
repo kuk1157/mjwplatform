0b88831270068c663593b0e3de2e0fc139ef3244
mjw7 <mjw@vpudding.com>
2025-08-08 09:49:53 +0900

    Fix: 프론트엔드 - socket 관련 설명 추가

--- frontend/socket-server.ts
+++ frontend/socket-server.ts
@@ -31,23 +31,25 @@ app.use(bodyParser.json()); // JSON 본문 파싱
 //     });
 // });
 
-// 백엔드에서 보낸 방문기록 수신 (REST 방식)
+// 소켓 기본 설정 - 소켓 연결시 바로 실행 될 세팅값
 io.on("connection", (socket) => {
     console.log("연결됨 :", socket.id);
 
-    // storeId를 받아서 해당 방에 입장
+    // 방문기록 실시간 출력 - 가맹점 별로 룸 생성 되고 소켓에 부여
     socket.on("joinStore", (storeId: number) => {
         const roomName = `store-${storeId}`;
         socket.join(roomName);
         console.log(`소켓 ${socket.id} 이 store-${storeId} 룸에 입장`);
     });
 
+    // 나중에 기능 추가시 여기에 넣기
+
     socket.on("disconnect", () => {
         console.log("연결 끊김", socket.id);
     });
 });
 
-// 방문기록 API
+// 방문기록 API - Spring VisitLogServiceImpl에서 방문기록 생성시 호출
 app.post("/api/socket/store-new-visit", (req, res) => {
     const { storeId, visitLog } = req.body;
 
