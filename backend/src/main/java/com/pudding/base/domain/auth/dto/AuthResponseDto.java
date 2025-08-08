9952cbad73817ca0a8055a335735647ce439d063
mjw7 <mjw@vpudding.com>
2025-08-08 10:23:42 +0900

    Fix: 백엔드 - 점주용 웹 로그인시 분기처리를 위한 role responseDto에 추가

--- backend/src/main/java/com/pudding/base/domain/auth/dto/AuthResponseDto.java
+++ backend/src/main/java/com/pudding/base/domain/auth/dto/AuthResponseDto.java
@@ -1,6 +1,7 @@
 package com.pudding.base.domain.auth.dto;
 
 import com.pudding.base.domain.auth.entity.Auth;
+import com.pudding.base.domain.member.enums.Role;
 import lombok.*;
 
 @Getter
@@ -11,11 +12,13 @@ public class AuthResponseDto {
     private String tokenType;
     private String accessToken;
     private String refreshToken;
+    private Role role;
 
     @Builder
     public AuthResponseDto(Auth entity) {
         this.tokenType = entity.getTokenType();
         this.accessToken = entity.getAccessToken();
         this.refreshToken = entity.getRefreshToken();
+        this.role = entity.getMember().getRole();
     }
 }
