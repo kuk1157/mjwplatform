3d659b059563ec21283ce889ddfbb7432c8b3a54
mjw7 <mjw@vpudding.com>
2025-09-01 13:13:04 +0900

    Feat: 프론트엔드 - 모바일 상단 타이틀 이미지 추가 및 컴포넌트화

---
+++ frontend/src/components/organisms/mobileMain.tsx
@@ -0,0 +1,11 @@
+const MobileMain = () => {
+    return (
+        <div className="mb-3 p-1">
+            <div className="flex items-center gap-2 mb-2">
+                <img src="/assets/image/did/mobileMain.svg" alt="메인 이미지" />
+            </div>
+        </div>
+    );
+};
+
+export { MobileMain };
