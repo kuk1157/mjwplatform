73df42bd7854ea5e9dcb2f471a9487337454ccea
mjw7 <mjw@vpudding.com>
2025-09-01 12:46:16 +0900

    Fix: 프론트엔드 - 모바일 다대구 로그인 디자인 수정

--- frontend/src/components/pages/mobile/login.tsx
+++ frontend/src/components/pages/mobile/login.tsx
@@ -11,23 +11,30 @@ export function MobileLogin() {
         <div className="w-screen h-screen flex items-center justify-center">
             <div className="w-[360px] bg-white text-center p-6 rounded-xl">
                 {/* 로고 */}
-                <div className="mb-6">
-                    <img
-                        src="/assets/image/did/icon_2.png"
-                        alt="mobileLogo"
-                        className="w-24 h-auto mx-auto"
+
+                <div className="pb-60">
+                    <div className="mb-6">
+                        <img
+                            src="/assets/image/did/icon_2.png"
+                            alt="mobileLogo"
+                            className="w-24 h-auto mx-auto"
+                        />
+                    </div>
+
+                    {/* 타이틀 */}
+                    <div className="mb-2 text-2xl font-bold font-Pretendard">
+                        동대구역 맛집 3% 할인
+                    </div>
+                    <div className="mb-4 text-2xl font-light font-Pretendard">
+                        다대구 DID로 간편하게!
+                    </div>
+                </div>
+                <div>
+                    <DidLoginButton
+                        storeNum={storeNum}
+                        tableNumber={tableNumber}
                     />
                 </div>
-
-                {/* 타이틀 */}
-                <div className="mb-2 text-xl font-bold">
-                    동대구역 맛집 3% 할인
-                </div>
-                <div className="mb-4 text-xl font-bold">
-                    다대구 DID로 간편하게!
-                </div>
-
-                <DidLoginButton storeNum={storeNum} tableNumber={tableNumber} />
 
                 {/* <button
                     className="w-full py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-800 transition"
