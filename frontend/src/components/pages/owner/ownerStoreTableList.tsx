83c47553a477f872d25fe16dd866f69c1425b3fa
mjw7 <mjw@vpudding.com>
2025-09-02 14:35:45 +0900

    Fix: 프론트엔드 - 매장 테이블 조회 디자인 수정

--- frontend/src/components/pages/owner/ownerStoreTableList.tsx
+++ frontend/src/components/pages/owner/ownerStoreTableList.tsx
@@ -39,18 +39,23 @@ function OwnerStoreTableList() {
     return (
         <MainContainer className="py-[230px] bg-[#F6F6F6] lg:py-[150px] sm:py-[100px]">
             <div className="min-h-screen bg-gray-50 p-8">
-                <div className="w-full min-w-[1200px] mx-auto">
+                <div className="w-full min-w-[1000px] mx-auto">
                     <div className="mb-6 flex justify-start">
                         <button
                             onClick={OwnerDashBoard}
-                            className="px-5 py-2 bg-yellow-400 text-white font-semibold rounded-lg shadow hover:bg-yellow-500 transition-colors"
+                            className="px-5 py-2 bg-[#E61F2C] text-white font-semibold rounded-lg"
                         >
-                            📑 대시보드로 이동
+                            대시보드로 이동
                         </button>
                     </div>
-                    <h1 className="text-3xl font-bold text-gray-800 mb-8">
-                        🍽️ 매장 테이블 조회
-                    </h1>
+                    <div className="text-3xl font-bold text-gray-800 mb-8">
+                        <img
+                            className="inline-block w-[45px] mr-1"
+                            src="/assets/image/dashboard/storeTable.svg"
+                            alt="매장 테이블 조회"
+                        />
+                        <span>매장 테이블 조회</span>
+                    </div>
                     <div className="grid grid-cols-4 gap-4 p-4">
                         {tables.length === 0 ? (
                             <div className="col-span-4 text-center text-gray-500 text-lg">
@@ -60,7 +65,7 @@ function OwnerStoreTableList() {
                             tables.map((table, index) => (
                                 <div
                                     key={index}
-                                    style={{ backgroundColor: "#21A089" }}
+                                    style={{ backgroundColor: "#E61F2C" }}
                                     className="w-40 h-40 rounded shadow flex flex-col items-center justify-center"
                                 >
                                     <p className="text-lg font-normal text-white">
