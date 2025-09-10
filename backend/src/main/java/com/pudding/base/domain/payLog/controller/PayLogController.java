f1f447bc6d279fefb314a541710191339fa3a843
mjw7 <mjw@vpudding.com>
2025-09-10 15:30:56 +0900

    Fix: 백엔드 - 결제 내역 페이징 적용 - Pageable 객체 활용 - 생성일 내림차순 정렬

--- backend/src/main/java/com/pudding/base/domain/payLog/controller/PayLogController.java
+++ backend/src/main/java/com/pudding/base/domain/payLog/controller/PayLogController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +34,7 @@ public class PayLogController {
 
     @Operation(summary = "점주 결제내역 조회", description ="점주용 웹 플랫폼 메인 대시보드 용도")
     @GetMapping("/owner/{ownerId}")
-    public ResponseEntity<Page<PayLogDto>> getOwnerIdByPayLogs(Pageable pageable, @PathVariable Integer ownerId){
+    public ResponseEntity<Page<PayLogDto>> getOwnerIdByPayLogs(@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable, @PathVariable Integer ownerId){
         Page<PayLogDto> payLogDto = payLogService.findByOwnerIdPayLogs(pageable, ownerId);
         return ResponseEntity.ok(payLogDto);
     }
