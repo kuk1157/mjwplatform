6818043ab51f0cbe10a2144121fb22c33c1ee6ba
mjw7 <mjw@vpudding.com>
2025-10-20 13:14:48 +0900

    Feat: 프론트엔드- 모바일 로그아웃 기능 작업

--- frontend/src/components/organisms/mobileMain.tsx
+++ frontend/src/components/organisms/mobileMain.tsx
@@ -1,8 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 interface MobileMainProps {
     param: number;
 }
+
 const MobileMain = ({ param }: MobileMainProps) => {
+    const navigate = useNavigate();
+
+    const logOutButton = () => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("customerId");
+        navigate("/mobile/mainPageTest");
+    };
     return (
         <div className="mb-3 p-1">
             <div className="flex items-center gap-2 mb-2 justify-between">
@@ -13,7 +21,15 @@ const MobileMain = ({ param }: MobileMainProps) => {
                     />
                 </Link>
                 {param ? (
-                    <div></div>
+                    <div>
+                        {" "}
+                        <button
+                            className="cursor-poiner flex items-center justify-center border border-[#E61F2C] rounded-[25px] py-[8px] px-[14.5px] text-[#E61F2C] font-Pretendard"
+                            onClick={logOutButton}
+                        >
+                            로그아웃
+                        </button>
+                    </div>
                 ) : (
                     <div>
                         <button
