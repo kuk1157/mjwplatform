6f00ca536d0d5a7fa7d8f664f051fe7f5ab9e860
mjw7 <mjw@vpudding.com>
2025-10-24 10:53:03 +0900

    Feat: 백엔드 - 네이버 지도 key 반환 api 작업
    - yml 저장 후 프론트에서 백엔드 api 호출하여 키 사용

---
+++ backend/src/main/java/com/pudding/base/naver/controller/NaverMapController.java
@@ -0,0 +1,27 @@
+package com.pudding.base.naver.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "네이버 지도 관련 API", description= "네이버 지도 관련 Controller")
+@RestController
+@Component
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/naver/maps")
+public class NaverMapController {
+
+    @Value("${NaverMap.client-ID}")
+    private String clientId;
+    @Operation(summary = "네이버 지도 clientID 추출", description = "yml에 저장되어 있는 값 추출")
+    @GetMapping()
+    public ResponseEntity<String> getNaverMapClientId(){
+        return ResponseEntity.ok(clientId);
+    }
+}
